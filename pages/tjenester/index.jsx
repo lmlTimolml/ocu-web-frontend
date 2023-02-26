3bfa247dc3b159f507f101fd58e737d230487b8e
Timo Westre <timowestre@gmail.com>
2023-02-26 11:22:04 +0100

    div updates graphql test

--- pages/tjenester/index.jsx
+++ pages/tjenester/index.jsx
@@ -1,7 +1,7 @@
-import Layout from "../components/layout/layout";
-import Hero from "../components/layout/hero";
-import { getServicesPageContent, getGlobalContent } from "../lib/api";
-import { customColors } from "../customdata";
+import Layout from "../../components/layout/layout";
+import Hero from "../../components/layout/hero";
+import { getServicesPageContent, getGlobalContent } from "../../lib/api";
+import { customColors } from "../../customdata";
 
 const background = customColors();
 

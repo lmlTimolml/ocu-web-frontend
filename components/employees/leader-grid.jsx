c718a9f58d0f1acc642388987adb8848673f37b9
Timo Westre <timowestre@gmail.com>
2023-02-27 11:02:45 +0100

    deleted unresolved path to component

--- components/employees/leader-grid.jsx
+++ components/employees/leader-grid.jsx
@@ -1,11 +1,10 @@
-import ImageTwoCol from "../modules/image-2-col";
 import EmployeeCard from "./employee-card";
 
 export default function LeaderGrid({ info }) {
   return (
     <ul className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 max-w-[960px] mx-auto" style={{ backgroundColor: `${background[bgcolor]}`}}>
       <li className="col-span-2 h-full min-h-[300px] relative">
-        <ImageTwoCol />
+        
       </li>
 
       {info.map(employee => {

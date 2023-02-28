0d5e676d5b70dcb5591907af2e7a366ee6cac5ca
Timo Westre <timowestre@gmail.com>
2023-02-28 08:55:08 +0100

    div clean up

--- pages/personvern/personvern.jsx
+++ pages/personvern/personvern.jsx
@@ -31,17 +31,6 @@ export default function personvernPage({ pageContent, globalContent }) {
         alt={alt}
         breadcrumbpath={breadcrumbpath}
       />
-      <section
-        className="py-5"
-        style={{ backgroundColor: `${background[bgcolor]}` }}
-      >
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 max-w-[960px] mx-5 lg:mx-auto">
-        {/* <div className="flex flex-col md:flex-row max-w-[960px] mx-5 lg:mx-auto justify-between"> */}
-          {servLink?.map((eventContent, i) => (
-            <FeaturedLeft key={i} eventContent={eventContent} />
-          ))}
-        </div>
-      </section>
 
       {components?.map((components, i) => {
         return <section key={i}>{components}</section>;

c3f3d2ac65a67622eeb70cab29f3e256b242202b
Timo Westre <timowestre@gmail.com>
2023-03-09 22:48:26 +0100

    spacing imggrid

--- components/modules/ComponentBlockImgGrid.jsx
+++ components/modules/ComponentBlockImgGrid.jsx
@@ -13,12 +13,12 @@ export default function ImgGrid({ logo, bgColor: { bgcolor } }) {
       <div className="mx-5 md:mx-10 lg:m-auto grid grid-cols-3 md:grid-cols-6 lg:grid-cols-12 gap-4 max-w-[1440px]">
         <div className="col-span-3 md:col-span-6 lg:col-start-3 lg:col-span-8 grid grid-cols-2 sm:grid-cols-4 gap-4 mt-4">
           {logo?.map((logo, i) => (
-              <Link key={i} href={logo.url} className="p-2 flex justify-center items-center relative">
+              <Link key={i} href={logo.url} className="p-4 flex justify-center items-center relative">
                 <Image
                   src={logo.logo.data.attributes.url}
                   width={150}
                   height={50}
-                  style={{ objectFit: "contain", maxHeight: "60px", width: "auto" }}
+                  style={{ objectFit: "contain", height: "auto", maxWidth: "140px" }}
                   alt={logo.alt}
                 />
               </Link>

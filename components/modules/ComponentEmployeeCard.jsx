d576b72abe6e058de6676834320a059ff0ac6f3b
Timo Westre <timowestre@gmail.com>
2023-03-14 01:22:18 +0100

    ansattkort justering

--- components/modules/ComponentEmployeeCard.jsx
+++ components/modules/ComponentEmployeeCard.jsx
@@ -5,33 +5,33 @@ import { customColors } from "../../customdata";
 const background = customColors();
 
 export default function EmployeeCard({ employee }) {
-
   const {
     name,
     title,
     img,
     alt,
     contact,
-    bgCard: {bgcolor}
+    bgCard: { bgcolor },
   } = employee.attributes;
 
   const imgurl = img.data[0].attributes.url;
 
   return (
-    <div className="flex flex-col h-full justify-between ease-linear duration-150" style={{ backgroundColor: `${background[bgcolor]}` }}>
-       {imgurl && <Image height={540} width={540} src={imgurl} alt={alt} />}
-      <div
-        className="flex flex-col justify-start w-full p-3">
-        {name && (
-          <h2 className="font-bold">{name}</h2>
-        )}
-        {title && <p className="mb-4">{title}</p>}
+    <div
+      className="flex flex-col h-full justify-between ease-linear duration-150"
+      style={{ backgroundColor: `${background[bgcolor]}` }}>
+    <div>
+      <Image height={540} width={540} src={imgurl} alt={alt} />
+      <div className="flex flex-col w-full p-3 mb-5">
+        {name && <h2 className="font-bold">{name}</h2>}
+        {title && <p className="sm:text-sm">{title}</p>}
       </div>
-<div className="flex pl-3">
+      </div>
+      <div className="flex pl-3">
         {contact?.map((contact, i) => {
-            return <Icon key={i} contact={contact} />
-          })}
-          </div>
+          return <Icon key={i} contact={contact} />;
+        })}
+      </div>
     </div>
   );
 }
